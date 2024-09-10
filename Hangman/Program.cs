e610b0edf52636e474c9fcd3bc5911018ffa2d46
Leopold Seidenglanz <leopold.seidenglanz@elev.ga.ntig.se>
2024-09-10 10:57:43 +0200

    Hej

---
+++ Hangman/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+Bestämma ord
+Visa ett _ för varje bokstav i ordet
+Ta in en gissning
+Om gissningen finns i ordet
+    Skriver på rätt plats (motsvarande bokstavens plats i ordet)
+annars
+    Lägger till bokstaven till de felaktiga gissningarna
+    Ritar ett steg till av gubben
+Fortsätt tills gubben dör eller ordet gissas rätt.w
+*/
+
+string word = "Volvo";
+
+char guess = Console.ReadLine()[0];
+/* 
+bool inWord = false;
+for (int i = 0; i < word.Length; i++)
+{
+    if (word[i] == guess)
+    {
+        inWord = true;
+    }
+}
+*/
+bool inWord = word.Contains(guess);
+
+if (inWord == true)
+{
+    Console.WriteLine("yay");
+}
+
+Console.ReadLine();
